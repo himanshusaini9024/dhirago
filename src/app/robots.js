c7dd4f9218c8e1e0cc9e0624ecda61f855c3c419
himanshusaini9024 <sainihimanshu208@gmail.com>
2026-06-12 12:46:53 +0530

    changes

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,17 +1,14 @@
 export default function robots() {
-  const isIndexingEnabled =
-    process.env.NEXT_PUBLIC_ALLOW_INDEXING === "true";
+  const isIndexingEnabled = process.env.NEXT_PUBLIC_ALLOW_INDEXING === "true";
 
   return {
     rules: {
       userAgent: "*",
-      allow: isIndexingEnabled ? "/" : "",
-      disallow: isIndexingEnabled
-        ? ["/cart", "/account", "/checkout","/"]
-        : "/",
+      allow: "/",
+      disallow: isIndexingEnabled ? ["/cart", "/account", "/checkout"] : "/",
     },
     sitemap: isIndexingEnabled
-      ? "https://dhirago-a6xq.vercel.app/sitemap.xml"
+      ? `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`
       : "",
   };
 }

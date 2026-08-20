1f9746df9ae585d31669fb203e58bf9e4a4e8c0b
himanshusaini9024 <sainihimanshu208@gmail.com>
2026-08-20 15:43:36 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.dhirago.com",
-          },
-        ],
-        destination: "https://dhirago.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
